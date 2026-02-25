2396d0a735494ff0786df98b9e29bebd64c55988
ZhouSicheng-2011 <ZSCinYBSZ2023@outlook.com>
2026-02-25 22:03:04 +0800

    Update Bit7zLibrary_EVP.cpp

--- src/Bit7zLibrary_EVP.cpp
+++ src/Bit7zLibrary_EVP.cpp
@@ -3,6 +3,7 @@ This file binds the Bit7zLibrary, the DLL provider of bit7z.
 (bit7z uses it to call the DLL of 7-zip, like 7z.dll, 7za.dll and so on)
 (For more details, see website https://github.com/rikyoz/bit7z/wiki/7z-DLLs)
 (To get the extra DLLs, download 7z-extra package from https://www.7-zip.org/a/7z2600-extra.7z)
+(Want other versions of 7-zip DLLs? Use the version number you want to replace "2600" in the above URL, for example, "2501" for 7-zip 25.01)
 Author: ZhouSicheng-2011
 Time: 2026-02-23
 License: This project is under the MIT Lincense, the whole text is after the source
@@ -12,5 +13,36 @@ License: This project is under the MIT Lincense, the whole text is after the sou
 #include <API.hpp>
 
 void init_lib(py::module& mod){
-    // Bind the Bit7zLibrary class
+    //Bind the default 7-zip dll of bit7z
+    mod.attr("DEFAULT_7ZIP_DLL") = py::cast(bit7z::kDefaultLibrary);
+
+    //Bind the Bit7zLibrary class
+    py::class_<bit7z::Bit7zLibrary>(mod, "Bit7zLibrary")
+        .def(py::init<const std::string&>())
+        .def("set_large_page_mode", &bit7z::Bit7zLibrary::setLargePageMode);
 }
+
+/*
+MIT License
+
+Copyright (c) 2025 ZhouSicheng-2011
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+*/
